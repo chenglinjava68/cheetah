68daeb9e6f961c248fcb0df13498149f0cbb0c1b
Max <huangzefeng@foreveross.com>
2016-02-16 11:23:21 +0800

    handler

---
+++ src/main/java/cheetah/distributor/handler/HandleCallbackWrap.java
@@ -0,0 +1,36 @@
+package cheetah.distributor.handler;
+
+import cheetah.distributor.EventMessage;
+
+/**
+ * Created by Max on 2016/2/16.
+ */
+public class HandleCallbackWrap {
+    private boolean exception;
+    private EventMessage eventMessage;
+    private Class<?> exceptionObject;
+    private String exceptionMessage;
+
+    public HandleCallbackWrap(boolean exception, EventMessage eventMessage, Class<?> exceptionObject, String exceptionMessage) {
+        this.exception = exception;
+        this.eventMessage = eventMessage;
+        this.exceptionObject = exceptionObject;
+        this.exceptionMessage = exceptionMessage;
+    }
+
+    public boolean isException() {
+        return exception;
+    }
+
+    public EventMessage getEventMessage() {
+        return eventMessage;
+    }
+
+    public Class<?> getExceptionObject() {
+        return exceptionObject;
+    }
+
+    public String getExceptionMessage() {
+        return exceptionMessage;
+    }
+}
