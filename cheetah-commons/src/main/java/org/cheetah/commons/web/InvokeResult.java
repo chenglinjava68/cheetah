3d8de53f5081916b90969f4b5a604361ffd34398
Max <pdemok@163.com>
2016-05-01 11:11:53 +0800

    加入web的工具封装

--- cheetah-commons/src/main/java/org/cheetah/commons/web/InvokeResult.java
+++ cheetah-commons/src/main/java/org/cheetah/commons/web/InvokeResult.java
@@ -7,23 +7,23 @@ import org.cheetah.commons.PlatformException;
  * 调用结果
  * Created by Max on 15/1/20.
  */
-public class Invoker<T> {
+public class InvokeResult<T> {
     private T data;
     private boolean hasErrors = false;
     private String errorCode = "";
     private String errorMsg = "";
 
-    private Invoker(T data) {
+    private InvokeResult(T data) {
         this.data = data;
     }
 
-    private Invoker(String errorCode, String errorMsg) {
+    private InvokeResult(String errorCode, String errorMsg) {
         this.errorCode = errorCode;
         this.errorMsg = errorMsg;
         this.hasErrors = true;
     }
 
-    private Invoker() {
+    private InvokeResult() {
 
     }
 
@@ -31,7 +31,7 @@ public class Invoker<T> {
         return data;
     }
 
-    public Invoker<T> data(T t) {
+    public InvokeResult<T> data(T t) {
         this.data = t;
         return this;
     }
@@ -64,23 +64,23 @@ public class Invoker<T> {
         return errorMsg;
     }
 
-    public static <T> Invoker<T> success(T data) {
-        return new Invoker<T>(data);
+    public static <T> InvokeResult<T> success(T data) {
+        return new InvokeResult<T>(data);
     }
 
-    public static <T> Invoker<T> failure(String errorCode) {
-        return new Invoker<T>(errorCode, "");
+    public static <T> InvokeResult<T> failure(String errorCode) {
+        return new InvokeResult<T>(errorCode, "");
     }
 
-    public static <T> Invoker<T> failure(String errorCode, String errorMsg) {
-        return new Invoker<T>(errorCode, errorMsg);
+    public static <T> InvokeResult<T> failure(String errorCode, String errorMsg) {
+        return new InvokeResult<T>(errorCode, errorMsg);
     }
 
-    public static <T> Invoker<T> failure(PlatformException e) {
-        return new Invoker<T>(e.getErrorCode(), e.getMessage());
+    public static <T> InvokeResult<T> failure(PlatformException e) {
+        return new InvokeResult<T>(e.getErrorCode(), e.getMessage());
     }
 
-    public static Invoker<Void> voidResult() {
-        return new Invoker<Void>();
+    public static InvokeResult<Void> voidResult() {
+        return new InvokeResult<Void>();
     }
 }
