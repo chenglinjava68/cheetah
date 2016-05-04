54e4162217f6762d06ae238024661e4efb5adcc8
Max <pdemok@163.com>
2016-05-04 13:12:38 +0800

    优化

--- cheetah-fighter/src/main/java/org/cheetah/fighter/worker/support/OrdinaryWorker.java
+++ cheetah-fighter/src/main/java/org/cheetah/fighter/worker/support/OrdinaryWorker.java
@@ -33,13 +33,10 @@ public class OrdinaryWorker implements Worker {
             future.get(3, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             e.printStackTrace();
-            handler.onFailure(directive);
         } catch (ExecutionException e) {
             e.printStackTrace();
-            handler.onFailure(directive);
         } catch (TimeoutException e) {
             e.printStackTrace();
-            handler.onFailure(directive);
         }
     }
 
