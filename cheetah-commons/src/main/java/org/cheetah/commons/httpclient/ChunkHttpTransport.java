cef313b788ee8417de54b94e33fcebe9edaf0b12
Max <pdemok@163.com>
2016-05-16 10:13:25 +0800

    优化

--- cheetah-commons/src/main/java/org/cheetah/commons/httpclient/ChunkHttpTransport.java
+++ cheetah-commons/src/main/java/org/cheetah/commons/httpclient/ChunkHttpTransport.java
@@ -2,7 +2,7 @@ package org.cheetah.commons.httpclient;
 
 import org.apache.http.client.HttpClient;
 
-import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by Max on 2015/11/26.
@@ -11,5 +11,5 @@ public interface ChunkHttpTransport {
 
     void download(HttpClient httpClient, String url, String toPath);
 
-    InputStream download(HttpClient httpClient, String url);
+    void  download(OutputStream stream, HttpClient httpClient, String url);
 }
