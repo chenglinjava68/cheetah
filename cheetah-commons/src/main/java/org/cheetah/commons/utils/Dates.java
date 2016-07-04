35daedc6d195aef02d03ec45173b3d834f8dc246
maxhuang <maxhuang@kugou.net>
2016-07-04 09:48:51 +0800

    优化

---
+++ cheetah-commons/src/main/java/org/cheetah/commons/utils/Dates.java
@@ -0,0 +1,55 @@
+package org.cheetah.commons.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Created by maxhuang on 2016/7/1.
+ */
+public final class Dates {
+    public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+
+    public static final String TIME_FORMAT = "HH:mm:ss";
+
+    private Dates(){}
+
+    public static String now() {
+        return now(DEFAULT_DATE_TIME_FORMAT);
+    }
+
+    public static String now(String format) {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(format));
+    }
+
+    public static String time() {
+        return time(TIME_FORMAT);
+    }
+
+    public static String time(String format) {
+        return LocalTime.now().format(DateTimeFormatter.ofPattern(format));
+    }
+
+    public static String date() {
+        return date(DATE_FORMAT);
+    }
+
+    public static String date(String format) {
+        return LocalDate.now().format(DateTimeFormatter.ofPattern(format));
+    }
+
+    public static int year(String date, String format) {
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern(format)).getYear();
+    }
+
+    public static int month(String date, String format) {
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern(format)).getMonthValue();
+    }
+
+    public static int day(String date, String format) {
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern(format)).getDayOfMonth();
+    }
+}
