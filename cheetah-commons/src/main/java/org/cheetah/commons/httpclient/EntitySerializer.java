3b153753c832e69692500f5ec384da42dce06d6b
maxhuang <maxhuang@kugou.net>
2016-07-08 16:32:02 +0800

    优化

--- cheetah-commons/src/main/java/org/cheetah/commons/httpclient/EntitySerializer.java
+++ cheetah-commons/src/main/java/org/cheetah/commons/httpclient/EntitySerializer.java
@@ -1,11 +1,25 @@
 package org.cheetah.commons.httpclient;
 
 /**
+ * 加=
+ *
  * Created by Max on 2016/7/6.
  */
 public interface EntitySerializer {
+    /**
+     * 将请求实体序列化
+     * @param entity
+     * @return
+     */
     String serialize(Object entity);
 
+    /**
+     * 将资源表述转为对象实体
+     * @param representation 资源表述
+     * @param entity    实体
+     * @param <T>   反序列化后的对象
+     * @return
+     */
     <T> T deserialize(String representation, Class<T> entity);
 
 }
--- cheetah-commons/src/main/java/org/cheetah/commons/httpclient/serializer/Jackson2JsonSerializer.java
+++ cheetah-commons/src/main/java/org/cheetah/commons/httpclient/serializer/Jackson2JsonSerializer.java
@@ -1,19 +1,44 @@
 package org.cheetah.commons.httpclient.serializer;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.cheetah.commons.httpclient.EntitySerializer;
-import org.cheetah.commons.web.JsonSerializer;
+
+import java.io.IOException;
 
 /**
+ * jackson序列化实现
+ *
  * Created by Max on 2016/7/6.
  */
 public class Jackson2JsonSerializer implements EntitySerializer {
+    private final static ObjectMapper objectMapper;
+
+    static {
+        objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+    }
+
     @Override
     public String serialize(Object entity) {
-        return JsonSerializer.serialize(entity);
+        try {
+            return objectMapper.writeValueAsString(entity);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
     public <T> T deserialize(String representation, Class<T> entity) {
-        return JsonSerializer.deserialize(representation, entity);
+        try {
+            return objectMapper.readValue(representation, entity);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
