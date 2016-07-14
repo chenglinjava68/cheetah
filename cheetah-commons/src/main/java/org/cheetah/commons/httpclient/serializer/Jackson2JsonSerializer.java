d19d924ff6e77541a43274a5076ce2cdc6caf2b6
Max <gpdemok@gmail.com>
2016-07-14 23:19:31 +0800

    优化

--- cheetah-commons/src/main/java/org/cheetah/commons/httpclient/serializer/Jackson2JsonSerializer.java
+++ cheetah-commons/src/main/java/org/cheetah/commons/httpclient/serializer/Jackson2JsonSerializer.java
@@ -2,8 +2,11 @@ package org.cheetah.commons.httpclient.serializer;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.cheetah.commons.httpclient.EntitySerializer;
 import org.cheetah.commons.httpclient.HttpClientException;
 
@@ -21,6 +24,9 @@ public class Jackson2JsonSerializer implements EntitySerializer {
         objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
         objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.configure(JsonGenerator.Feature.IGNORE_UNKNOWN, true);
     }
 
     @Override
