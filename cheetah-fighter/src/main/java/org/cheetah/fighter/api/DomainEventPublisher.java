a221a57a9a8547fdf6a0ad0822a3ffe4f61a4d0c
maxhuang <maxhuang@kugou.net>
2016-08-01 15:43:53 +0800

    添加注释

--- cheetah-fighter/src/main/java/org/cheetah/fighter/api/DomainEventPublisher.java
+++ cheetah-fighter/src/main/java/org/cheetah/fighter/api/DomainEventPublisher.java
@@ -17,18 +17,44 @@ public abstract class DomainEventPublisher {
     private DomainEventPublisher() {
     }
 
+    /**
+     * 最为简单的发布，无法知道执行结果，disruptor和future都支持
+     * @param event
+     * @param <E>
+     */
     public static <E extends DomainEvent> void publish(E event) {
         collector.collect(event);
     }
 
+    /**
+     * 发布后可以等待事件结果，需要将feedback设为true，如果需要知道事件消费执行的结果需要使用Future引擎，
+     * 如果使用disruptor,得到的eventresult是一个无法知道后续情况值
+     * @param event
+     * @param feedback  仅支持future引擎
+     * @return
+     */
     public static <E extends DomainEvent> EventResult publish(E event, boolean feedback) {
         return collector.collect(event, feedback);
     }
-
+    /**
+     * 发布后可以等待事件结果，需要将feedback设为true，并且支持消费者执行的超时时间设置，如果需要知道事件
+     * 消费执行的结果需要使用Future引擎，如果使用disruptor,得到的eventresult是一个无法知道后续情况值
+     * @param event
+     * @param feedback  仅支持future引擎
+     * @param timeout   仅支持future引擎
+     * @return
+     */
     public static <E extends DomainEvent> EventResult publish(E event, boolean feedback, int timeout) {
         return collector.collect(event, feedback, timeout);
     }
-
+    /**
+     * 发布后可以等待事件结果，需要将feedback设为true，如果需要知道事件消费执行的结果需要使用Future引擎，
+     * 如果使用disruptor,得到的eventresult是一个无法知道后续情况值
+     * @param event
+     * @param feedback  仅支持future引擎
+     * @param timeout   仅支持future引擎
+     * @return
+     */
     public static <E extends DomainEvent> EventResult publish(E event, boolean feedback, int timeout, TimeUnit timeUnit) {
         return collector.collect(event, feedback, timeout, timeUnit);
     }
