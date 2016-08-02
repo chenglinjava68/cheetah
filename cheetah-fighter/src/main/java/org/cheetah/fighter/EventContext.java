8e6ed6fd24fc97c69913b245894a641790b51390
Max <gpdemok@gmail.com>
2016-08-02 14:53:16 +0800

    优化eventcontext

--- cheetah-fighter/src/main/java/org/cheetah/fighter/EventContext.java
+++ cheetah-fighter/src/main/java/org/cheetah/fighter/EventContext.java
@@ -11,19 +11,19 @@ public final class EventContext {
     /**
      * 本次触发事件中所有的消费者被包装为Handler
      */
-    private static final ThreadLocal<List<Handler>> handlers = new ThreadLocal<>();
+    private final ThreadLocal<List<Handler>> handlers = new ThreadLocal<>();
     /**
      * 配置
      */
-    private static final ThreadLocal<FighterConfig> fighterConfig = new ThreadLocal<>();
+    private final ThreadLocal<FighterConfig> fighterConfig = new ThreadLocal<>();
     /**
      * 本次触发事件的事件消息
      */
-    private static final ThreadLocal<EventMessage> eventMessage = new ThreadLocal<>();
+    private final ThreadLocal<EventMessage> eventMessage = new ThreadLocal<>();
     /**
      * 本次触发事件中的拦截器
      */
-    private static final ThreadLocal<List<Interceptor>> interceptors = new ThreadLocal<>();
+    private final ThreadLocal<List<Interceptor>> interceptors = new ThreadLocal<>();
 
     private static final EventContext CONTEXT = new EventContext();
     private EventContext(){}
@@ -33,50 +33,50 @@ public final class EventContext {
     }
 
     public final void setHandlers(List<Handler> handlers) {
-        EventContext.handlers.set(handlers);
+        this.handlers.set(handlers);
     }
 
     public final List<Handler> getHandlers() {
-        return EventContext.handlers.get();
+        return this.handlers.get();
     }
 
     public final void removeHandlers() {
-        EventContext.handlers.remove();
+        this.handlers.remove();
     }
 
     public final void setFighterConfig(FighterConfig fighterConfig) {
-        EventContext.fighterConfig.set(fighterConfig);
+        this.fighterConfig.set(fighterConfig);
     }
 
     public final FighterConfig getFighterConfig() {
-        return EventContext.fighterConfig.get();
+        return this.fighterConfig.get();
     }
 
     public final void removeFighterConfig() {
-        EventContext.fighterConfig.remove();
+        this.fighterConfig.remove();
     }
 
     public final void setEventMessage(EventMessage $eventMessage) {
-        EventContext.eventMessage.set($eventMessage);
+        this.eventMessage.set($eventMessage);
     }
 
     public final EventMessage getEventMessage() {
-        return EventContext.eventMessage.get();
+        return this.eventMessage.get();
     }
 
     public final void removeEventMessage() {
-        EventContext.eventMessage.remove();
+        this.eventMessage.remove();
     }
 
     public final void setInterceptor(List<Interceptor> interceptors) {
-        EventContext.interceptors.set(interceptors);
+        this.interceptors.set(interceptors);
     }
 
     public final List<Interceptor> getInterceptors() {
-        return EventContext.interceptors.get();
+        return this.interceptors.get();
     }
 
     public final void removeInterceptor() {
-        EventContext.interceptors.remove();
+        this.interceptors.remove();
     }
 }
