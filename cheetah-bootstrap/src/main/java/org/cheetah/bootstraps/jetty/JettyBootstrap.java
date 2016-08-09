e19601640727f2675bdcf547979c9ef7b79dad60
maxhuang <maxhuang@kugou.net>
2016-08-09 11:13:55 +0800

    升级jetty9

--- cheetah-bootstrap/src/main/java/org/cheetah/bootstraps/jetty/JettyBootstrap.java
+++ cheetah-bootstrap/src/main/java/org/cheetah/bootstraps/jetty/JettyBootstrap.java
@@ -1,5 +1,7 @@
 package org.cheetah.bootstraps.jetty;
 
+import org.apache.tomcat.InstanceManager;
+import org.apache.tomcat.SimpleInstanceManager;
 import org.cheetah.bootstraps.BootstrapException;
 import org.cheetah.bootstraps.BootstrapSupport;
 import org.cheetah.common.logger.Err;
@@ -9,9 +11,13 @@ import org.cheetah.common.utils.StringUtils;
 import org.cheetah.configuration.Configuration;
 import org.cheetah.configuration.ConfigurationFactory;
 import org.cheetah.ioc.spring.web.CheetahContextLoaderListener;
-import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.annotations.ServletContainerInitializersStarter;
+import org.eclipse.jetty.apache.jsp.JettyJasperInitializer;
+import org.eclipse.jetty.jsp.JettyJspServlet;
+import org.eclipse.jetty.plus.annotation.ContainerInitializer;
+import org.eclipse.jetty.server.ConnectionFactory;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
@@ -21,8 +27,14 @@ import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import javax.servlet.Servlet;
 import java.io.File;
-import java.util.EnumSet;
-import java.util.EventListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.*;
+
+import static org.eclipse.jetty.http.HttpParser.LOG;
 
 /**
  * @author Max
@@ -51,9 +63,10 @@ public class JettyBootstrap extends BootstrapSupport {
     private String applicationConfig;
     protected JettyServerConfig serverConfig;
     private Server server;
-    private SelectChannelConnector connector;
+    private ServerConnector connector;
     protected WebAppContext webAppContext;
     protected Class<? extends Servlet> dispatcher;
+    private URI serverURI;
 
     public JettyBootstrap() {
         initialize();
@@ -84,15 +97,16 @@ public class JettyBootstrap extends BootstrapSupport {
     @Override
     protected void startup() throws Exception {
         try {
-            QueuedThreadPool queuedThreadPool = new QueuedThreadPool(this.serverConfig.maxThreads());
-            server = new Server();
-            server.setThreadPool(queuedThreadPool);
-            configServerConnector();
+            server = new Server(new QueuedThreadPool(this.serverConfig.maxThreads(), this.serverConfig.minThreads()));
+            ServerConnector connector = configServerConnector();
+            server.addConnector(connector);
 
-            configWebAppContext();
+            configWebAppContext(getScratchDir());
 
             server.setHandler(webAppContext);
             server.start();
+
+            this.serverURI = getServerUri(connector);
         } catch (Exception e) {
             throw new BootstrapException("jetty boot occurs error.", e);
         }
@@ -159,21 +173,38 @@ public class JettyBootstrap extends BootstrapSupport {
         stop();
     }
 
-    private void configServerConnector() {
-        connector = new SelectChannelConnector();
+    /**
+     * 配置服务连接器
+     * @return
+     */
+    private ServerConnector configServerConnector() {
+        connector = new ServerConnector(server);
         connector.setPort(serverConfig.port());
-        connector.setMaxIdleTime(serverConfig.timeout());
+        connector.setIdleTimeout(serverConfig.timeout());
         connector.setAcceptQueueSize(serverConfig.acceptQueueSize());
         if (StringUtils.isNotBlank(serverConfig.host())) {
             connector.setHost(serverConfig.host());
         }
         connector.setReuseAddress(true);
 
-        server.addConnector(connector);
+        return connector;
     }
 
-    private void configWebAppContext() {
+    /**
+     * 配置web上下文
+     * @param scratchDir
+     */
+    private void configWebAppContext(File scratchDir) {
+
+        webAppContext.setAttribute("javax.servlet.context.tempdir", scratchDir);
+
         webAppContext.setContextPath(serverConfig.contextPath());    //设置上下文根路径
+
+        webAppContext.setAttribute("org.eclipse.jetty.containerInitializers", Arrays.asList (
+                new ContainerInitializer(new JettyJasperInitializer(), null)));
+        webAppContext.setAttribute(InstanceManager.class.getName(), new SimpleInstanceManager());
+        webAppContext.addBean(new ServletContainerInitializersStarter(webAppContext), true);
+
         setContextLoaderListener();
         FilterHolder filterHolder = createEncodingFilter();
         if (Objects.nonNull(filterHolder))
@@ -189,6 +220,41 @@ public class JettyBootstrap extends BootstrapSupport {
         }
 
         webAppContext.setParentLoaderPriority(true);
+    }
+
+    /**
+     * Establish Scratch directory for the servlet context (used by JSP compilation)
+     */
+    private File getScratchDir() throws IOException {
+        File tempDir = new File(System.getProperty("java.io.tmpdir"));
+        File scratchDir = new File(tempDir.toString(), "embedded-jetty-jsp");
+
+        if (!scratchDir.exists()) {
+            if (!scratchDir.mkdirs()) {
+                throw new IOException("Unable to create scratch directory: " + scratchDir);
+            }
+        }
+        return scratchDir;
+    }
+
+    /**
+     * Establish the Server URI
+     */
+    private URI getServerUri(ServerConnector connector) throws URISyntaxException {
+        String scheme = "http";
+        for (ConnectionFactory connectFactory : connector.getConnectionFactories()) {
+            if (connectFactory.getProtocol().equals("SSL-http")) {
+                scheme = "https";
+            }
+        }
+        String host = connector.getHost();
+        if (host == null) {
+            host = "localhost";
+        }
+        int port = connector.getLocalPort();
+        serverURI = new URI(String.format("%s://%s:%d/", scheme, host, port));
+        Info.log(JettyBootstrap.class, "Server URI: " + serverURI);
+        return serverURI;
     }
 
     private String getWebappPath(String[] webappPaths) {
@@ -250,4 +316,15 @@ public class JettyBootstrap extends BootstrapSupport {
         }
     }
 
+
+    /**
+     * Cause server to keep running until it receives a Interrupt.
+     * <p>
+     * Interrupt Signal, or SIGINT (Unix Signal), is typically seen as a result of a kill -TERM {pid} or Ctrl+C
+     *
+     * @throws InterruptedException if interrupted
+     */
+    public void waitForInterrupt() throws InterruptedException {
+        server.join();
+    }
 }
