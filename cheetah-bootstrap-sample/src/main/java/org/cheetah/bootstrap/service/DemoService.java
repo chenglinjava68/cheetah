b3ecb75b396baca01cfae0bb6a3d1f0578df0ac1
Max <gpdemok@gmail.com>
2016-08-13 19:47:28 +0800

    优化

--- cheetah-bootstrap-sample/src/main/java/org/cheetah/bootstrap/service/DemoService.java
+++ cheetah-bootstrap-sample/src/main/java/org/cheetah/bootstrap/service/DemoService.java
@@ -1,8 +1,14 @@
 package org.cheetah.bootstrap.service;
 
+import java.util.List;
+
 /**
  * Created by Max on 2016/7/26.
  */
 public interface DemoService {
     String get();
+
+    List<String> list();
+
+    Integer get(String name);
 }
--- cheetah-bootstrap-sample/src/main/java/org/cheetah/bootstrap/service/DemoServiceImpl.java
+++ cheetah-bootstrap-sample/src/main/java/org/cheetah/bootstrap/service/DemoServiceImpl.java
@@ -1,5 +1,9 @@
 package org.cheetah.bootstrap.service;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 /**
  * Created by Max on 2016/7/26.
  */
@@ -7,5 +11,15 @@ public class DemoServiceImpl implements DemoService {
     @Override
     public String get() {
         return "test";
+    }
+
+    @Override
+    public List<String> list() {
+        return Lists.newArrayList("a", "b");
+    }
+
+    @Override
+    public Integer get(String name) {
+        return 123;
     }
 }
